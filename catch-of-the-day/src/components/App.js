6c17c7e3c6223f705c61a7f03f2c41a002959596
Todd <tpolasek@gmail.com>
2016-11-05 13:03:58 -0700

    end of video 16

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
 		super();
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 		//getinitialstate
 		this.state = {
 			fishes: {},
@@ -36,6 +37,15 @@ class App extends React.Component {
 		});
 	}
 
+	addToOrder(key){
+		//take copy of state
+		const order = {...this.state.order};
+		//update or add the new number of fish ordered
+		order[key] = order[key] + 1 || 1;
+		this.setState({ order });
+
+	}
+
 	render(){
 		return (
 			<div className="catch-of-the-day">
@@ -45,11 +55,11 @@ class App extends React.Component {
 					{
 						Object
 						.keys(this.state.fishes)
-						.map(key => <Fish key={key} details={this.state.fishes[key]} />)
+						.map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
 					}
 					</ul>
 				</div>
-				<Order />
+				<Order  />
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples} />
 			</div>
 
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 class Order extends React.Component {
 	render(){
 		return(
