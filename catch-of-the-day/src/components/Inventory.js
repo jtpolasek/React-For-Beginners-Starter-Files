2abaac89d6f551cbca345c8bba7c9c1c5e2647c6
Todd <tpolasek@gmail.com>
2016-11-14 09:42:32 -0800

    done with auth vid 24

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -10,15 +10,29 @@ class Inventory extends React.Component {
 		this.renderLogin = this.renderLogin.bind(this);
 		this.authenticate = this.authenticate.bind(this);
 		this.authHandler = this.authHandler.bind(this);
+		this.logout = this.logout.bind(this);
 		this.state = {
 			uid: null,
 			owner: null
 		}
 	}
 
+	componentDidMount(){
+		base.onAuth((user) => {
+			if(user) {
+				this.authHandler(null, {user});
+			}
+		})
+	}
+
 	authenticate(provider){
 		console.log(`Trying to log in with ${provider}`)
 		base.authWithOAuthPopup(provider, this.authHandler);
+	}
+
+	logout(){
+		base.unauth();
+		this.setState({uid: null});
 	}
 
 	authHandler(err, authData){
@@ -89,7 +103,7 @@ class Inventory extends React.Component {
   }
 
 	render(){
-		const logout = <button>Log Out</button>
+		const logout = <button onClick={this.logout}>Log Out</button>
 		//check if they are not logged in at all
 		if(!this.state.uid) {
 				return <div>{this.renderLogin()}</div>
